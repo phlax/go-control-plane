7607cf75cc7c55afb787c853cb8594368fc007c1
Ryan Northey <ryan@synca.io>
2023-05-29 10:48:40 +0100

    .github/

--- ci/sync_envoy.sh
+++ ci/sync_envoy.sh
@@ -6,6 +6,8 @@ MIRROR_MSG="Mirrored from envoyproxy/envoy"
 SRCS=(envoy contrib)
 GO_TARGETS=(@envoy_api//...)
 IMPORT_BASE="github.com/envoyproxy/go-control-plane"
+COMMITTER_NAME="envoy-sync[bot]"
+COMMITTER_EMAIL="envoy-sync[bot]@users.noreply.github.com"
 
 
 if [[ -z "$ENVOY_SRC_DIR" ]]; then
@@ -83,6 +85,8 @@ commit_changes () {
     echo "$changes"
     latest_commit="$(git -C "${ENVOY_SRC_DIR}" rev-list "${last_envoy_sha}"..HEAD | head -n1)"
     echo "$latest_commit" > envoy/COMMIT
+    git config --global user.email "$COMMITTER_EMAIL"
+    git config --global user.name "$COMMITTER_NAME"
     git add envoy contrib
     git commit --allow-empty -s -m "${MIRROR_MSG} @ {latest_commit}"
     git push origin main
